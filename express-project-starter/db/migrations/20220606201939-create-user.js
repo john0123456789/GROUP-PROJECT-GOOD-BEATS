dc99d2704ba6b3cf3d3d7056336e60b0390c1ca7
BrenLau <brenl@DESKTOP-K01VFCG.localdomain>
2022-06-06 16:31:54 -0400

    created users db, table, and seed

---
+++ express-project-starter/db/migrations/20220606201939-create-user.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(50)
+      },
+      emailAddress: {
+        unique: true,
+        allowNull: false,
+        type: Sequelize.STRING(100)
+      },
+      hashedPassword: {
+        allowNull: false,
+        type: Sequelize.STRING.BINARY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
---
+++ express-project-starter/db/seeders/20220606202431-UserDemo.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.bulkInsert('Users', [{
+      username: 'Leroy Jenkins',
+      emailAddress: 'hisnameatsomethingdotcom@netscape.com',
+      hashedPassword: 'password',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};

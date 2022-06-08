37bb9a9c9d8a79cc69cf73bf981e14771a282616
Michael Presley <mipresley23@gmail.com>
2022-06-08 17:15:58 -0400

    review routes and form

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,10 +3,14 @@ const { nextTick } = require('process');
 const db = require('../db/models');
 const router = express.Router();
 const { csrfProtection, asyncHandler } = require('./utils');
+const {requireAuth} = require('../auth.js');
 
-router.get()
+router.get('/new', csrfProtection, requireAuth, async(req, res) => {
+  const reviews = await db.Review.findAll();
+  res.render('reviews', {csrfToken: req.csrfToken(), data: {}, reviews})
+})
 
-router.post()
+//router.post()
 
 
 

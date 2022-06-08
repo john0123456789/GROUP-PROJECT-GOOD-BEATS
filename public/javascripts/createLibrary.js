b718f1acb83268b5ad12fb2432f009934e79d5f7
Irving Arreola Palacios <irving.a789@gmail.com>
2022-06-08 18:59:53 -0500

    added click event listener

--- public/javascripts/createLibrary.js
+++ public/javascripts/createLibrary.js
@@ -1,3 +1,4 @@
+// import db from '../../db/models'
 
 
 window.addEventListener('DOMContentLoaded', (e) => {
@@ -7,18 +8,35 @@ window.addEventListener('DOMContentLoaded', (e) => {
       const libraryName = document.createElement('input')
       const submitButton = document.createElement('button')
       const cancelButton = document.createElement('button')
+      const inputContainer = document.getElementById('input-container');
+      cancelButton.innerText = 'Cancel';
+      submitButton.innerText = 'Add'
+      submitButton.addEventListener('click', async(e) => {
+          //getting value of the input the user typed in 
+        
+        const name = libraryName.value;
+        const userId = document.getElementsByClassName('hidden')[0].value;
+        console.log(userId)
+         //creating a library with the name from above
+        // await db.Library.create({
+        //     name,
+        //     userId
+        // })
+
+      })
+      cancelButton.addEventListener('click', () => {
+        inputContainer.removeChild(submitButton)
+        inputContainer.removeChild(libraryName)
+        inputContainer.removeChild(cancelButton)
+      })
+      if(inputContainer.childNodes.length < 2){
+          inputContainer.appendChild(libraryName)
+          inputContainer.appendChild(submitButton)
+          inputContainer.appendChild(cancelButton)
+
+      }
 
 
-    //   submitButton.addEventListener('click', async(e) => {
-    //       //getting value of the input the user typed in 
-    //     const name = libraryName.value;
-    //     const userId = document.getElementsByClassName('hidden')[0].value;
-
-    //      //creating a library with the name from above
-
-
-    //   })
-      console.log('in add library')
   })
 })
 

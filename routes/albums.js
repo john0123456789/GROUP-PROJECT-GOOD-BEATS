5717b71ec20b6ee027b3211b5fc8fb0f347c2bd0
BrenLau <brenl@DESKTOP-K01VFCG.localdomain>
2022-06-09 14:12:16 -0400

    addedalbums

--- routes/albums.js
+++ routes/albums.js
@@ -5,36 +5,37 @@ const router = express.Router();
 const { csrfProtection, asyncHandler } = require('./utils');
 const { requireAuth } = require('../auth.js')
 
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
   const albums = await db.Album.findAll();
-  res.render('albums', {albums})
+  res.render('albums', { albums })
 })
 
 
-router.get('/:id(\\d+)',csrfProtection, asyncHandler(async(req, res) => {
+router.get('/:id(\\d+)', csrfProtection, asyncHandler(async (req, res) => {
 
   const users = await db.User.findAll();
 
   const album = await db.Album.findByPk(req.params.id);
-  const {userId} = req.session.auth
-  const libraries = await db.Library.findAll({where: {userId}})
+  const { userId } = req.session.auth
+  const libraries = await db.Library.findAll({ where: { userId } })
   const trackList = album.trackLists.split(',')
   console.log('reqParams: ', req.params)
   const reviews = await db.Review.findAll({
     where: {
 
       albumId: req.params.id
-    }})
-  res.render('albumEach', { album, trackList, reviews, libraries, csrfToken: req.csrfToken()})
+    }
+  })
+  res.render('albumEach', { album, trackList, reviews, libraries, csrfToken: req.csrfToken() })
 }))
 
-router.get('/:id(\\d+)/reviews/new', csrfProtection, requireAuth, async(req, res) => {
+router.get('/:id(\\d+)/reviews/new', csrfProtection, requireAuth, async (req, res) => {
   const album = await db.Album.findByPk(req.params.id);
   const reviews = await db.Review.findAll();
-  res.render('reviews', {csrfToken: req.csrfToken(), reviews, album})
+  res.render('reviews', { csrfToken: req.csrfToken(), reviews, album })
 })
 
-router.post('/:id(\\d+)', csrfProtection, requireAuth, asyncHandler(async(req, res, next) => {
+router.post('/:id(\\d+)', csrfProtection, requireAuth, asyncHandler(async (req, res, next) => {
   const { title, content, rating, userId, albumId } = req.body;
   const review = await db.Review.create({
     title,
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { csrfProtection, asyncHandler } = require('./utils');
+const db = require('../db/models');
+const { requireAuth } = require('../auth.js')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+  const { userId } = req.session.auth
+  const libraries = await db.Library.findAll({ where: { userId } })
+  res.render('index', { title: 'goodbeats', libraries });
+}));
 
 module.exports = router;

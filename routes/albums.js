826d97d431c18f3bf2d90d2a8b757a9123cd0e85
Michael Presley <mipresley23@gmail.com>
2022-06-09 12:22:39 -0400

    finished functionality for GET and POST reviews

--- routes/albums.js
+++ routes/albums.js
@@ -3,6 +3,7 @@ const { nextTick } = require('process');
 const db = require('../db/models');
 const router = express.Router();
 const { csrfProtection, asyncHandler } = require('./utils');
+const { requireAuth } = require('../auth.js')
 
 router.get('/', async(req, res) => {
   const albums = await db.Album.findAll();
@@ -24,6 +25,24 @@ router.get('/:id(\\d+)', asyncHandler(async(req, res) => {
     }
   })
   res.render('albumEach', { album, trackList, reviews })
+}))
+
+router.get('/:id(\\d+)/reviews/new', csrfProtection, requireAuth, async(req, res) => {
+  const album = await db.Album.findByPk(req.params.id);
+  const reviews = await db.Review.findAll();
+  res.render('reviews', {csrfToken: req.csrfToken(), reviews, album})
+})
+
+router.post('/:id(\\d+)', csrfProtection, requireAuth, asyncHandler(async(req, res, next) => {
+  const { title, content, rating, userId, albumId } = req.body;
+  const review = await db.Review.create({
+    title,
+    content,
+    rating,
+    userId: req.session.auth.userId,
+    albumId: req.params.id
+  })
+  return res.redirect(`/albums/${req.params.id}`)
 }))
 
 
--- routes/reviews.js
+++ routes/reviews.js
@@ -6,28 +6,7 @@ const { csrfProtection, asyncHandler } = require('./utils');
 const {requireAuth} = require('../auth.js');
 
 
-router.get('/', async(req, res) => {
-  const reviews = await db.Review.findAll();
-  res.send(reviews);
-});
 
-router.get('/new', csrfProtection, requireAuth, async(req, res) => {
-  const albums = await db.Album.findAll();
-  const reviews = await db.Review.findAll();
-  res.render('reviews', {csrfToken: req.csrfToken(), reviews, albums})
-})
-
-router.post('/', csrfProtection, requireAuth, asyncHandler(async(req, res, next) => {
-  const { title, content, rating, userId, albumId } = req.body;
-  const review = await db.Review.create({
-    title,
-    content,
-    rating,
-    userId: req.session.auth.userId,
-    albumId
-  })
-  return res.redirect(`/`)
-}))
 
 
 

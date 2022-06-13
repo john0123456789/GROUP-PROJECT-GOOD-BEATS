007049aff0cc551d3ea605fd288677db2208fe13
Irving Arreola Palacios <irving.a789@gmail.com>
2022-06-12 21:12:18 -0500

    added styles to create library button and form

--- public/javascripts/createLibrary.js
+++ public/javascripts/createLibrary.js
@@ -6,14 +6,15 @@ window.addEventListener('DOMContentLoaded', (e) => {
   
   addLibrary.addEventListener('click', () => {
     //   const libraryName = document.createElement('input')
-      const submitButton = document.getElementById('add')
       const cancelButton = document.getElementById('cancel')
+      addLibrary.classList.add("hidden")
     //   const inputContainer = document.getElementById('input-container');
      const form = document.getElementById('library-form');
      form.classList.remove('hidden');
       cancelButton.addEventListener('click', async(e) => {
         e.preventDefault();
         form.classList.add('hidden')
+        addLibrary.classList.remove('hidden')
       })
   
 
